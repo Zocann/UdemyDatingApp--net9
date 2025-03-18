689a4d6364e7ed6230bcb0b1ac10d71eafcfac32
Karel Mareth <mareth.karel@gmail.com>
2025-03-18 11:24:59 +0100

    End of section 6

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
-export const routes: Routes = [];
+//authGuard - protecting from acessing components in client
+//dummy route - having child routes that inherit authGuard and url from it
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    //dummy route
+    {path: '', 
+        runGuardsAndResolvers: 'always', 
+        canActivate: [authGuard], 
+        children: [
+            {path: 'members', component: MemberListComponent},
+            {path: 'members/:id', component: MemberDetailComponent},
+            {path: 'lists', component: ListsComponent},
+            {path: 'messages', component: MessagesComponent},
+        ]
+    },
+    
+    {path: '**', component: HomeComponent, pathMatch: 'full'}
+];
